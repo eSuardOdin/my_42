2d2317229b0d229fb1498e8e35d2f4f17cc60881
eSuardOdin <esuardodin@gmail.com>
2024-07-24 14:53:08 +0200

    boring

--- c02/ex07/ft_strupcase.c
+++ c02/ex07/ft_strupcase.c
@@ -1,13 +1,29 @@
 /*
-    Autorisé :
-    But : 
-    Prototype : 
+    Autorisé : Aucune
+    But : Écrire une fonction qui met en majuscule chaque lettre.
+    Prototype : char *ft_strupcase(char *str);
 */
 #include <stdio.h>
 #include <unistd.h>
 
+char *ft_strupcase(char *str)
+{
+    while(*str != '\0')
+    {
+        if((int)*str >= 97 && (int)*str <= 122)
+        {
+            *str = (int)*str - 32;
+        }
+        str++;
+    }
+    return str;
+}
+
 int main()
 {
-
+    char str[25] = "Ceci est un Test !";
+    printf("%s\n", str);
+    ft_strupcase(str);
+    printf("%s\n", str);
     return 0;
 }
--- c02/ex08/ft_strlowcase.c
+++ c02/ex08/ft_strlowcase.c
@@ -1,13 +1,29 @@
 /*
-    Autorisé :
-    But : 
-    Prototype : 
+    Autorisé : Aucune
+    But : Écrire une fonction qui met en minuscule chaque lettre.
+    Prototype : char *ft_strlowcase(char *str);
 */
 #include <stdio.h>
 #include <unistd.h>
 
+char *ft_strlowcase(char *str)
+{
+    while(*str != '\0')
+    {
+        if((int)*str >= 65 && (int)*str <= 90)
+        {
+            *str = (int)*str + 32;
+        }
+        str++;
+    }
+    return str;
+}
+
 int main()
 {
-
+    char str[25] = "Ceci est un Test !";
+    printf("%s\n", str);
+    ft_strlowcase(str);
+    printf("%s\n", str);
     return 0;
 }
