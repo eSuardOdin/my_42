c2185e53313cac059cbb215cd92db9cfe7707cf6
eSuardOdin <esuardodin@gmail.com>
2024-07-28 15:06:02 +0200

    ok

---
+++ c03/ex00/ft_strcmp.c
@@ -0,0 +1,14 @@
+/*
+    Autorisé : write()
+    But : Reproduire à l’identique le fonctionnement de la fonction strcmp (man strcmp).
+
+    Prototype : int ft_strcmp(char *s1, char *s2);
+*/
+#include <stdio.h>
+#include <unistd.h>
+
+
+int ft_strcmp(char *s1, char *s2)
+{
+
+}
