82b4f633a2a44ece14d8cb1447f9c1a263847b01
eSuardOdin <esuardodin@gmail.com>
2024-07-30 14:42:53 +0200

    Before tests

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -1,5 +1,5 @@
 /*
-    Autorisé : write()
+    Autorisé :
     But : Reproduire à l’identique le fonctionnement de la fonction strcmp (man strcmp).
 
     Prototype : int ft_strcmp(char *s1, char *s2);
@@ -8,7 +8,23 @@
 #include <unistd.h>
 
 
+
 int ft_strcmp(char *s1, char *s2)
 {
-
+    while(*s1 != '\0' && *s2 != '\0')
+    {
+        if(*s1 != *s2)
+        {
+            return (int)*s1 - (int)*s2;
+        }
+        s1++;
+        s2++;
+    }
+    // If end of the two strings
+    if(*s1 == *s2)
+    {
+        return 0;
+    }
+    // If s1 shorter, return negative, else return positive.
+    return (*s1 == '\0') ? -1 : 1;
 }
