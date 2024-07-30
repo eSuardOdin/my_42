99d0d01aa4889ea45c18a0c07d7d38def107dca5
eSuardOdin <esuardodin@gmail.com>
2024-07-30 17:32:01 +0200

    c03/ex01 ok

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -1,27 +1,33 @@
 /*
     Autorisé :
-    But : Reproduire à l’identique le fonctionnement de la fonction strcmp (man strcmp).
+    But : Reproduire à l’identique le fonctionnement de la fonction strncmp (man strncmp).
 
-    Prototype : int ft_strcmp(char *s1, char *s2);
+    Prototype : int ft_strncmp(char *s1, char *s2, unsigned int n);
 */
 #include <stdio.h>
 #include <unistd.h>
 
 
 
-int ft_strcmp(char *s1, char *s2)
+int ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-    while(*s1 != '\0' && *s2 != '\0')
+    unsigned int i = 0;
+    while(*s1 != '\0' && *s2 != '\0' && i < n)
     {
+        printf("Testing %c vs %c -> i : %d/%d\n", *s1, *s2, i, n-1);
         if(*s1 != *s2)
         {
             return (int)*s1 - (int)*s2;
         }
         s1++;
         s2++;
+        if(i < n)
+        {
+            i++;
+        }
     }
     // If end of the two strings
-    if(*s1 == *s2)
+    if(*s1 == *s2 || i == n)
     {
         return 0;
     }
@@ -32,10 +38,9 @@ int ft_strcmp(char *s1, char *s2)
 int main()
 {
 
-    char *str_a = "ab1";
-    // char *str_b = "abcd";
-    char *str_b = "abc";
-    int cp = ft_strcmp(str_a, str_b); 
+    char *str_a = "abcde123";
+    char *str_b = "abcdefgh";
+    int cp = ft_strncmp(str_a, str_b, 27); 
     if(!cp)
     {
         printf("Strings \"%s\" and \"%s\" are equal -> %d\n", str_a, str_b, cp);
