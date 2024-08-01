1da42ec90b7bac35f618f1f70320168a041e5a6e
eSuardOdin <esuardodin@gmail.com>
2024-08-01 05:33:17 +0200

    c04/ex01 ok

--- c04/ex01/ft_putstr.c
+++ c04/ex01/ft_putstr.c
@@ -1,15 +1,29 @@
 /*
-    Autorisé :
-    But : 
-    Prototype : 
+    Autorisé : write()
+    But : Écrire une fonction qui affiche un à un les caractères d’une chaîne à l’écran.
+        L’adresse du premier caractère de la chaîne est contenue dans le pointeur passé en
+        paramètre à la fonction.
+    Prototype : void ft_putstr(char *str);
 */
 #include <stdio.h>
 #include <unistd.h>
 
-
+void ft_putstr(char *str)
+{
+    while (*(str) != '\0')
+    {
+        write(1, str++, 1);
+    }
+}
 
 int main()
 {
-    
+    char *again = "Hello, again\n";
+    char last[20] = "Lastly, hello !\n";
+    ft_putstr("Hello, world");
+    printf("\n");
+    ft_putstr(again);
+    ft_putstr(last);
+
     return 0;
 }
