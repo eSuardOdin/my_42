377921024811fa3ad243af90529318951941594d
eSuardOdin <esuardodin@gmail.com>
2024-08-03 04:32:40 +0200

    c04/ex04 ok

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -32,57 +32,98 @@ int ft_strlen(char *str)
 }
 
 
-int is_char_repeated(char *str)
+/**
+ * @brief Checks if a base is valid
+ * 
+ * @param str The base to test
+ * @return int The lenght of the base or zero if not valid
+ */
+int is_base_valid(char *str)
 {
-    printf("testing string %s\n", str);
+    // Check if + or - sign already tested
+    int isCharTested = 0;
     int len = ft_strlen(str);
+
+    if(len <= 1)
+    {
+        return 0;
+    }
+
     for(int i = 0; i < len-1; i++)
     {
         for(int y = i + 1; y < len; y++)
         {
-            printf("testing '%c' vs '%c'\n", str[i], str[y]);
+            if(!isCharTested && (str[i] == '+' || str[i] == '-' || str[y] == '+' || str[y] == '-'))
+            {
+                return 0;
+            }
             if(str[i] == str[y])
             {
-                printf("Same char !\n");
-                return 1;
+                return 0;
             }
         }
+        // Don't test chars validity more than once
+        if(!isCharTested)
+        {
+            isCharTested = 1;
+        }
     }
-    return 0;
+    return len;
 }
 
 
-// void ft_putnbr_base(int nbr, char *base)
-// {
-//     // Get base and exit if not valid
-//     int base = ft_strlen(base);
+void ft_putnbr_base(int nbr, char *base)
+{
+    char ret = '\n';
+    char minus = '-';
 
-// }
+    // Handle neg
+    if(nbr < 0)
+    {
+        nbr = -nbr;
+        write(1, &minus, 1);
+    }
+    // Get base and exit if not valid
+    int base_len = is_base_valid(base);
+    if(!base_len) return;
+
+
+    // // ---- Debug -----
+    // printf("Getting %d to base %d\n", nbr, base_len);
+    // // -- End Debug ---
+
+    // Get maximum power dividing the number
+    int pow = 1;
+    while(nbr / pow > base_len)
+    {
+        pow *= base_len;
+    }
+
+    // // ---- Debug -----
+    // printf("Highest power of %d is %d\n", base_len, pow);
+    // // -- End Debug ---
+
+
+    // Get number to base
+    int unit;
+    // char unit_c;
+    while(nbr || pow > 1)
+    {
+        unit = nbr / pow;
+        write(1, &base[unit], 1);
+        nbr = nbr % pow;
+        pow /= base_len;
+    }
+
+    write(1, &ret, 1);
+}
 
 
 int main()
 {
-    char *ok = "abcdef";
-    int ok_i = is_char_repeated(ok);
-    if(ok_i)
-    {
-        printf("String not ok\n");
-    }
-    else
-    {
-        printf("Ok\n");
-    }
-
-    char *nok = "ab1cdefedf1";
-    int nok_i = is_char_repeated(nok);
-    if(nok_i)
-    {
-        printf("String not ok\n");
-    }
-    else
-    {
-        printf("Ok\n");
-    }
-
+    char *bin = "01";
+    ft_putnbr_base(24, bin);
+    char *oct = "azertyui";
+    ft_putnbr_base(-767, oct);
     return 0;
 }
