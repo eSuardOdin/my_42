7ee7afc89b128650ec1d8f756961a564d8842b40
eSuardOdin <esuardodin@gmail.com>
2024-08-12 10:03:48 +0200

    c06/ex02 ok

--- c06/ex02/ft_rev_params.c
+++ c06/ex02/ft_rev_params.c
@@ -1,13 +1,24 @@
 /*
     Autorisé :
-    But :
+    But : • Écrire un programme qui affiche les arguments reçus en ligne de commande.
+          • • Un par ligne dans l’ordre inverse de la ligne de commande.
+          • Vous devez afficher tous les arguments, sauf argv[0].
     Prototype :
 */
 #include <stdio.h>
 #include <unistd.h>
 
-int main()
+
+int main(int argc, char **argv)
 {
-    
+    char ret = '\n';
+    for(int i = argc - 1; i > 0; i--)
+    {
+        while(*(argv[i]))
+        {
+            write(1, argv[i]++, 1);
+        }
+        write(1, &ret, 1);
+    }
     return 0;
 }
